a9f7ed470f89ebb1e366cc17b33e4a2fbc2f1aab
hliang <huangliang@gmail.com>
2013-12-30 16:02:02 +0800

    分页插件更新。

--- README.md
+++ README.md
@@ -9,14 +9,15 @@
 基于工作及项目需要，开发本框架。
 
 ## Requirement
-* PHP 5.4+
-* 使用到数据库连接时（MySQL、SQLite、PostgreSQL、Oracle），需要PDO支持
+* LeaPHP只支持PHP5.4.0及以上版本，并支持最新的PHP5.5。
+* LeaPHP的运行需要APC模块，请自行前往PECL进行下载安装。
+* 使用到数据库连接时（MySQL、SQLite、PostgreSQL、Oracle），需要PDO支持。
 
 ## TODO
 
 ## Copyright and License
 
 ## See Also
-* [LeaPHP Framework 项目主页](http://leaphp.net)
+* [LeaPHP Framework 项目主页](http://hliang0813.github.io/leaphp/)
 * [LeaPHP Framework Wiki](https://github.com/hliang0813/leaphp1/wiki)
 
--- sysplugins/PageNav/init.plugin.php
+++ sysplugins/PageNav/init.plugin.php
@@ -6,6 +6,7 @@
  * 例：
  * $obj = new PageNav(new LeapDB());
  * $obj->sql('select * from table limit 10');
+ * // $obj->setLookup($lookup = 5);
  * $pageinfo = $obj->exec($page = 1);
  * 
  * @author hliang
@@ -26,6 +27,11 @@ class PageNav extends Base {
 	private $_current_page = 0;
 	private $_start_record = 0;
 	private $_limit_record = 10;
+	private $_pagelist_lookup = 5;
+	
+	// 分页数据信息
+	private $_total_record = 0;
+	private $_total_page = 0;
 	
 	/**
 	 * 构造函数，获取一个LeapDB的数据库操作对象
@@ -69,6 +75,15 @@ class PageNav extends Base {
 		return $this;
 	}
 	
+	public function setLookup($lookup = 5) {
+		$lookup = intval($lookup);
+		if ($lookup <= 0) {
+			throw new LeapException(LeapException::leapMsg(__METHOD__, 'Lookup number must be a POSITIVE NUMBER'));
+		}
+		
+		$this->_pagelist_lookup = $lookup;
+	}
+	
 	/**
 	 * 执行分页查询操作，进行再次数据库查询
 	 * 第一次，查询需要页面的数据集合
@@ -82,7 +97,7 @@ class PageNav extends Base {
 	 * @return Array
 	 */
 	public function exec($page = 1) {
-		$page = intval($page);
+		$page = intval($page) == 0 ? 1 : intval($page);
 		if ($page <= 0) {
 			throw new LeapException(LeapException::leapMsg(__METHOD__, 'Requested page number must be a POSITIVE NUMBER.'));
 		}
@@ -90,19 +105,58 @@ class PageNav extends Base {
 		$this->_current_page = $page;
 		$this->_start_record = ($page - 1) * $this->_limit_record;
 		
-		$_data_set = $this->queryDataList();
-		$_count = $this->queryCount();
+		$this->_total_record = $this->queryCount();
+		$this->_total_page = ceil($this->_total_record/$this->_limit_record);
+		$_data_set = ($this->_current_page > $this->_total_page) ? array() : $this->queryDataList();
 		
 		$_page_info = array(
 			'data' => $_data_set,
 			'info' => array(
-				'total_record' => $_count,
-				'total_page' => ceil($_count/$this->_limit_record),
+				'total_record' => $this->_total_record,
+				'total_page' => $this->_total_page,
 				'current_page' => $page,
+				'page_list' => $this->getPageList(),
 			),
 		);
 		
 		return $_page_info;
+	}
+	
+	/**
+	 * 获取页码列表
+	 * 
+	 * @author hliang
+	 * @since 1.0.0
+	 * 
+	 * @return multitype:
+	 */
+	private function getPageList() {
+		
+		$_pagelist_ary = array();
+		
+		if ($this->_current_page > $this->_total_page) {
+			return $_pagelist_ary;
+		}
+
+		for ($i = $this->_current_page - $this->_pagelist_lookup; $i <= $this->_current_page + $this->_pagelist_lookup; $i ++) {
+			if ($i <= 0) {
+				continue;
+			}
+			array_push($_pagelist_ary, $i);
+			if ($i >= $this->_total_page) {
+				break;
+			}
+		}
+		
+		if ($_pagelist_ary[0] > 1) {
+			array_unshift($_pagelist_ary, '...');
+		}
+		
+		if ($_pagelist_ary[count($_pagelist_ary) - 1] < $this->_total_page) {
+			array_push($_pagelist_ary, '...');
+		}
+		
+		return $_pagelist_ary;
 	}
 	
 	/**
