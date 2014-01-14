22c202e07f49ce2f76662fb034b2ced15042087c
hliang <huangliang@gmail.com>
2014-01-14 19:23:12 +0800

    補充詳細註釋。

--- core/Model.Class.php
+++ core/Model.Class.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * 數據庫模型對應類
+ * 
+ * @author hliang
+ * @package leaphp
+ * @subpackage core
+ * @since 1.0.0
+ *
+ */
 class Model {
 	protected $primary_key = 'id';
 	protected $keys = array();
@@ -7,6 +16,14 @@ class Model {
 	private $_all_keys = array();
 	private $_object = NULL;
 
+	/**
+	 * 構造函數，初始化一些內部對象
+	 * 
+	 * @author hliang
+	 * @since 1.0.0
+	 * 
+	 * @throws LeapException
+	 */
 	public function __construct() {
 		if (!$this->primary_key) {
 			throw new LeapException(LeapException::leapMsg(__METHOD__, '需要为数据模型指定一个主键。'));
@@ -19,16 +36,43 @@ class Model {
 		$this->_object = ORM::for_table($this->_table)->use_id_column($this->primary_key);
 	}
 
+	/**
+	 * 獲取ORM對象，高級操作時使用
+	 * 
+	 * @author hliang
+	 * @since 1.0.0
+	 * 
+	 * @return ORM
+	 */
 	public function obj() {
 		return $this->_object;
 	}
 
+	/**
+	 * 向數據庫增加一條記錄
+	 * 
+	 * @author hliang
+	 * @since 1.0.0
+	 * 
+	 * @param array $data
+	 * @return boolean
+	 */
 	public function save(array $data) {
 		$_o = $this->_object->create();
 		$_o->set($data);
 		return $_o->save();
 	}
 
+	/**
+	 * 從數據庫中刪除一條記錄
+	 * 
+	 * @author hliang
+	 * @since 1.0.0
+	 * 
+	 * @param string $condition
+	 * @throws LeapException
+	 * @return boolean
+	 */
 	public function delete($condition = NULL) {
 		$_o = $this->_object;
 
@@ -51,6 +95,17 @@ class Model {
 		}
 	}
 
+	/**
+	 * 從數據庫中修改一條記錄
+	 * 
+	 * @author hliang
+	 * @since 1.0.0
+	 * 
+	 * @param array $data
+	 * @param string $condition
+	 * @throws LeapException
+	 * @return boolean
+	 */
 	public function update(array $data, $condition = NULL) {
 		$_o = $this->_object;
 		
