e5024fa7a360ab1fd059477e97ada3ce29077bf6
hliang <huangliang@gmail.com>
2014-01-15 14:37:18 +0800

    未完成的BuildInApp修改。

--- sysplugins/BuildInApp/Apps/BuildInCommon.Class.php
+++ sysplugins/BuildInApp/Apps/BuildInCommon.Class.php
@@ -1,13 +1,6 @@
 <?php
 class BuildInCommon {
-	static private $db = NULL;
 	static public function initDb() {
-		$logger = LeapLogger::getLogger(__METHOD__);
-
-		if (!is_object(self::$db)) {
-			$logger->info('初始化数据库对象。');
-			self::$db = new LeapDB('master', 'administrator');
-		}
-		return self::$db;
+		ORM::configure(LeapDB::configure('master', 'administrator'));
 	}
 }
--- sysplugins/BuildInApp/Apps/administrator/login.Class.php
+++ sysplugins/BuildInApp/Apps/administrator/login.Class.php
@@ -27,6 +27,8 @@ class login extends Controller {
 		} else {
 			self::loginPage();
 		}
+		
+// 		BuildInCommon::initDb();
 	}
 
 	// 登录页面
@@ -34,32 +36,44 @@ class login extends Controller {
 		if (filter_input(INPUT_SERVER, 'REQUEST_METHOD') == 'POST') {
 			$username = filter_input(INPUT_POST, 'username');
 			$password = filter_input(INPUT_POST, 'password');
-
-			// 初始化数据库对象
-			$db = BuildInCommon::initDb();
 			
-			// 开启事务
-			$db->beginTransaction();
-			// 执行登录查询
-			$user = $db->execute('SELECT u_id, u_name, u_uptime, u_lasttime FROM lpfba_users WHERE u_name = ? AND u_pass = ?', array(
-				$username, md5($password),
-			))[0];
-			// 修改最后登录时间
-			$db->execute('UPDATE lpfba_users SET u_lasttime = ? WHERE u_name = ?', array(
-				mktime(), $username,
-			));
+			LeapImport::file(SYSPLUGIN_DIR . DS . 'BuildInApp' . DS . 'Models' . DS . 'lpf_users.Model.php');
 
-			// 如果登录成功，提交事务
-			if ($user) {
-				$result = $db->commit();
-			}
+			ORM::configure(LeapDB::configure('master', 'administrator'));
+			$user = new lpf_users();
+			
+			ORM::get_db()->beginTransaction();
+			
+			$login_result = $user->obj()->where_equal('u_name', $username)->where_equal('u_pass', md5($password))->find_one();
+			$upd_result = $user->update(array(
+					'u_lasttime' => mktime(),
+			), $login_result->id());
+			
+			
+// 			// 初始化数据库对象
+			
+// 			// 开启事务
+// // 			$db->beginTransaction();
+// 			// 执行登录查询
+// 			$user = $db->execute('SELECT u_id, u_name, u_uptime, u_lasttime FROM lpfba_users WHERE u_name = ? AND u_pass = ?', array(
+// 				$username, md5($password),
+// 			))[0];
+// 			// 修改最后登录时间
+// 			$db->execute('UPDATE lpfba_users SET u_lasttime = ? WHERE u_name = ?', array(
+// 				mktime(), $username,
+// 			));
+
+// 			// 如果登录成功，提交事务
+// 			if ($user) {
+// 				$result = $db->commit();
+// 			}
 
 			// 如果事务提交成功，修改登录状态
-			if ($result && self::changeLoginState($user)) {
-				self::redirect(leapJoin(ENTRY_URI, '/administrator/main/'));
-			} else {
-				self::redirect();
-			}
+// 			if ($result && self::changeLoginState($user)) {
+// 				self::redirect(leapJoin(ENTRY_URI, '/administrator/main/'));
+// 			} else {
+// 				self::redirect();
+// 			}
 
 			exit;
 		}
