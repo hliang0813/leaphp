f3ed7a75f91a789f6e5935e41166a88195dd3ab5
hliang <huangliang@gmail.com>
2014-02-19 15:34:52 +0800

    更新。

--- sysplugins/JSON/init.plugin.php
+++ sysplugins/JSON/init.plugin.php
@@ -1,5 +1,7 @@
 <?php
 class JSON {
+	static private $tips_error = 'JSON解码错误：';
+
 	static public function encode($value, $options = 0) {
 		return json_encode($value, $options);
 	}
@@ -11,22 +13,22 @@ class JSON {
 				return Base::response($string, NULL);
 				break;
 			case JSON_ERROR_DEPTH:
-				return Base::response('JSON解码错误：到达了最大堆栈深度', TRUE);
+				return Base::response(self::$tips_error . '到达了最大堆栈深度', TRUE);
 				break;
 			case JSON_ERROR_STATE_MISMATCH:
-				return Base::response('JSON解码错误：无效或异常的JSON', TRUE);
+				return Base::response(self::$tips_error . '无效或异常的JSON', TRUE);
 				break;
 			case JSON_ERROR_CTRL_CHAR:
-				return Base::response('JSON解码错误：控制字符错误，可能是编码不对', TRUE);
+				return Base::response(self::$tips_error . '控制字符错误，可能是编码不对', TRUE);
 				break;
 			case JSON_ERROR_SYNTAX:
-				return Base::response('JSON解码错误：语法错误', TRUE);
+				return Base::response(self::$tips_error . '语法错误', TRUE);
 				break;
 			case JSON_ERROR_UTF8:
-				return Base::response('JSON解码错误：异常的 UTF-8 字符，也许是因为不正确的编码', TRUE);
+				return Base::response(self::$tips_error . '异常的UTF-8字符，也许是因为不正确的编码', TRUE);
 				break;
 			default:
-				return Base::response('JSON解码错误：未知错误', TRUE);
+				return Base::response(self::$tips_error . '未知错误', TRUE);
 				break;
 		}
 	}
